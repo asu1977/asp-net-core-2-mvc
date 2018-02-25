71d5ecefb314f2c1323daf8c0631a9b2d31a39ba
asu1977 <asu1977@gmail.com>
2018-02-25 13:04:44 +0300

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace suppliers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
